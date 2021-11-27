3290a27b89e8c0f631432c51ee6f8cc44f9f58cb
Will Eccles <will@eccles.dev>
2021-11-27 13:35:37 -0500

    add index generation

--- gen.sh
+++ gen.sh
@@ -7,14 +7,15 @@ trap 'rm -f .contents' EXIT
 for post in src/*.md; do
   nodir="${post#src/}"
   dest="${nodir%.md}.html"
-  printf "generating %s\n" "$dest"
-  pandoc -s --template _template.html -o "p/$dest" "$post"
+  printf "generating entry: %s\n" "$dest"
+  pandoc -s --template gen/entry.html -o "p/$dest" "$post"
   metadata="$(head -n 1 "p/$dest")"
   metadata="${metadata%-->}"
   metadata="${metadata#<!--}"
   printf "%s@@%s\n" "$metadata" "$dest" >>.contents
 done
 
-# TODO sort the .contents file
-# use this to generate the site index
-awk 'BEGIN {FS="@@"} {print $1" - <a href=\"p/"$3"\">"$2"</a>"}' .contents
+printf "generating index.html\n"
+sort -r .contents \
+  | awk 'BEGIN {FS="@@"} {print $1" - <a href=\"p/"$3"\">"$2"</a><br/>"}' \
+  | cat gen/index_pre.html - gen/index_post.html >test_index.html
